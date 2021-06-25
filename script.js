1a2a6866ec69240e2111e4802e36d1cbf662a9c1
paulinel-cloud <paulinemm520@gmail.com>
2021-06-25 04:31:12 +0000

    try fixing appearance things

--- script.js
+++ script.js
@@ -7,15 +7,17 @@ function compute()
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
     
+    //validation for principal inputs, if amt is less than or equal to 0, will reject input 
     if (principal <= 0) {
         alert("Enter a positive number")
         document.getElementById("principal").focus();
-    } else {
+    } else { //otherwise if valid input, will return accumulated interest text
         document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>at an interest rate of <mark>" +
         rate + "%</mark>.<br/>You will receive an amount of <mark>" + interest + "</mark>,<br/>in the year <mark>" + year.toString() + "</mark>";
     }
 }
 
+//function to update the interest rate when slider is moved/changes
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
