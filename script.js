14af7ab838c3994d0b4345c80504bb64714dd092
paulinel-cloud <paulinemm520@gmail.com>
2021-06-25 04:15:35 +0000

    testing exercise 4

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
+    
+    if (principal <= 0) {
+        alert("Enter a positive number")
+        document.getElementById("principal").focus();
+    } else {
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>at an interest rate of <mark>" +
+        rate + "%</mark>.<br/>You will receive an amount of <mark>" + interest + "</mark>,<br/>in the year <mark>" + year.toString() + "</mark>";
+    }
 }
 
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval+'%';
 }
         
